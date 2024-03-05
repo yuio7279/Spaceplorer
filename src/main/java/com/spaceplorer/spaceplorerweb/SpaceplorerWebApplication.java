05ebff6702611a04d1da7015284b9cc575a4ead8
yuio7279 <yuio7279@gmail.com>
2024-03-05 17:38:00 +0900

    init commit

---
+++ src/main/java/com/spaceplorer/spaceplorerweb/SpaceplorerWebApplication.java
@@ -0,0 +1,13 @@
+package com.spaceplorer.spaceplorerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceplorerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceplorerWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spaceplorer/spaceplorerweb/SpaceplorerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceplorer.spaceplorerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceplorerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
