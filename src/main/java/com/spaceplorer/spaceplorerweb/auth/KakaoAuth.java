a7297c36d2e42505c03fd9d8363976849e396237
yuio7279 <yuio7279@gmail.com>
2024-03-07 01:10:36 +0900

    Feat: Auth 인터페이스, NaverAuth, KakaoAuth 추가
    - Authorization server에 접근하기 위한 URL을 생성
    - 추가확장이 될 것을 고려해 추상화했다.
    
    -

---
+++ src/main/java/com/spaceplorer/spaceplorerweb/auth/KakaoAuth.java
@@ -0,0 +1,31 @@
+package com.spaceplorer.spaceplorerweb.auth;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Getter
+@ToString
+public class KakaoAuth implements Auth {
+
+    private final String clientId;
+    private final String redirectUri;
+    private final String responseType;
+    private final String scope;
+    private final String authorizationUrl;
+    private final String AUTHORIZATION_REQUEST_URL_FORMAT_KAKAO = "%s?client_id=%s&redirect_uri=%s&response_type=%s&scope=%s";
+
+
+    public String getAuthorizationRequestUrl(){
+
+        String authorizationRequestUrl = String.format(AUTHORIZATION_REQUEST_URL_FORMAT_KAKAO,
+                authorizationUrl, clientId, redirectUri, responseType, scope);
+
+        log.info("method=getAuthorizationRequestUrl, authorizationRequestUrl={}",authorizationRequestUrl);
+        return authorizationRequestUrl;
+    }
+
+}
