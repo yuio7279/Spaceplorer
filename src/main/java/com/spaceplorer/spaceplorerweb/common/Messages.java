c6763a48f0b01cde2fd44002927b20f65a066377
yuio7279 <yuio7279@gmail.com>
2024-03-09 01:59:57 +0900

    Feat : 메시지 포멧 추가
    -
    
    -

--- src/main/java/com/spaceplorer/spaceplorerweb/common/Messages.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/common/Messages.java
@@ -8,6 +8,16 @@ public class Messages {
 
     //TOKEN
     public final static String NOT_FOUND_TOKEN= "토큰을 찾을 수 없습니다.";
+    public final static String EXPIRED_TOKEN= "토큰을 찾을 수 없습니다.";
+    public final static String INVALID_TOKEN= "유효하지 않은 토큰 입니다.";
+    public final static String INVALID_TOKEN_SIGNATURE= "유효하지 않은 토큰 서명입니다..";
+    public final static String UNSUPPORTED_TOKEN= "지원하지 않는 토큰 입니다.";
+    public final static String CREATED_TOKEN= "토큰이 생성 되었습니다.";
+
+    //COOKIE
+    public final static String NOT_FOUND_COOKIES= "쿠키를 찾을 수 없습니다.";
+    public final static String FOUNT_COOKIES= "발견한 쿠키 = {}";
+    public final static String UNSUPPORTED_COOKIES= "지원하지 않는 쿠키 입니다.";
 
 
 }
