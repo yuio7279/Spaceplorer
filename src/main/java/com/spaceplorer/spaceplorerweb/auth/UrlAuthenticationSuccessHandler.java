5a179d3a0e59f615eb59edfb94d358add4aff87b
yuio7279 <yuio7279@gmail.com>
2024-03-09 02:06:20 +0900

    Feat : 커스텀 SuccessHander, OAuth2UserService 추가
    - 작성은 했지만 지금 시점은 동작하지 않는다. 시큐리티 버전에 따른 특정한 설정이 필요한지 확인 후 사용/제거 할 것이다.
    
    -

---
+++ src/main/java/com/spaceplorer/spaceplorerweb/auth/UrlAuthenticationSuccessHandler.java
@@ -0,0 +1,40 @@
+package com.spaceplorer.spaceplorerweb.auth;
+
+import com.spaceplorer.spaceplorerweb.domain.Role;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UrlAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication){
+        String userName = authentication.getName();
+        Role role = getRole(authentication);
+
+        log.info("인증 되었습니다. user {}, role {}",userName,role);
+
+        String createdToken = tokenProvider.createToken(userName, role);
+
+        tokenProvider.addJwtToCookie(createdToken,response);
+        response.addHeader("Authorization", createdToken);
+    }
+
+    private Role getRole(Authentication authentication) {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
+        return Role.valueOf(role);
+    }
+}
