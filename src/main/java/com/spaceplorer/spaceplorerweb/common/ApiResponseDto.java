5f055ff9f66ab4223e71c5d25044472948745bda
yuio7279 <yuio7279@gmail.com>
2024-03-09 20:21:08 +0900

    Refactor : 응답DTO객체에 T data를 담아 함께 전달
    - data에는 정보가 담긴 객체이다.
    
    -

--- src/main/java/com/spaceplorer/spaceplorerweb/common/ApiResponseDto.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/common/ApiResponseDto.java
@@ -2,13 +2,22 @@ package com.spaceplorer.spaceplorerweb.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@RequiredArgsConstructor
-public class ApiResponseDto {
+public class ApiResponseDto<T> {
+    private  T data;
     private final String message;
     private final Integer statusCode;
 
+    public ApiResponseDto(T data, String message, Integer statusCode) {
+        this.data = data;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    public ApiResponseDto(String message, Integer statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
 }
