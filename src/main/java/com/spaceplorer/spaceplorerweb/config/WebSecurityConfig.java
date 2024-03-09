167108295025ddd358553cdfa05f3fa57d5fe431
yuio7279 <yuio7279@gmail.com>
2024-03-09 16:44:28 +0900

    Refactor : 인가 페이지 임시 허용 설정
    - API 개발을 위해 api 접근 허용 설정
    
    -

--- src/main/java/com/spaceplorer/spaceplorerweb/config/WebSecurityConfig.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/config/WebSecurityConfig.java
@@ -1,15 +1,25 @@
 package com.spaceplorer.spaceplorerweb.config;
 
+import com.spaceplorer.spaceplorerweb.auth.UrlAuthenticationSuccessHandler;
 import com.spaceplorer.spaceplorerweb.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
@@ -19,8 +29,8 @@ import org.springframework.security.web.firewall.StrictHttpFirewall;
 @Slf4j
 public class WebSecurityConfig {
 
-    private final CustomOAuth2UserService customOAuth2UserService;
-
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
 
     /**
      * WebSecurityConfigrerAdapter는 더이상 사용되지 않는다 5.7~
@@ -33,52 +43,53 @@ public class WebSecurityConfig {
         //세션방식에서는 세션이 항상 고정되기 때문에 csrf공격을 반드시 방어해주어야 한다.
         //jwt 방식은 세션을 stateless 상태로 관리하기 때문에 csrf에 대한 공격을 방어 할 필요는 없다.
         http
-                .csrf((auth) -> auth.disable());
+                .csrf(AbstractHttpConfigurer::disable);
 
         //Form 로그인 방식 disable
         //jwt방식으로 로그인 하기 때문에 form로그인을 사용하지 않는다.
-/*        http
-                .formLogin((auth) -> auth.disable());*/
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
 
         //http basic 인증 방식 disable
         http
-                .httpBasic((auth) -> auth.disable());
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         //경로별 인가
         http
+
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .loginPage("/login")
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                                .userService(oAuth2UserService))
+                        .successHandler(authenticationSuccessHandler)
+                        .failureUrl("/login/fail")
+                )
                 .authorizeHttpRequests((auth) -> auth
                         //static
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        //form
-                        .requestMatchers("/", "/auth/**","/test","index.html").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        //api
-                        /*.requestMatchers("/api/**").permitAll()*/
 
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(oauth2Login -> oauth2Login
-                        .loginPage("/auth/login")
-                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
-                                .userService(customOAuth2UserService))
-                        //.successHandler(urlAuthenticationSuccessHandler) 안됨,,
-                        .defaultSuccessUrl("/auth/login/success")
-                        .failureUrl("/auth/login/fail")
+                        //form
+                        .requestMatchers("/","/index.html","/permitAllContents.html").permitAll()
+                        .requestMatchers("/login/**","/logout/**","/error").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+
+                        //api
+                        .requestMatchers("/api/**").permitAll()
+
+                        .anyRequest().permitAll()
                 )
                 .logout(logout -> logout
-                                        .logoutUrl("/auth/logout") // 로그아웃 처리 URL
-                                        .logoutSuccessUrl("/test") // 로그아웃 성공 후 리다이렉트할 URL
+                                        .logoutUrl("/logout/success") // 로그아웃 처리 URL
+                                        .logoutSuccessUrl("/") // 로그아웃 성공 후 리다이렉트할 URL
                                         .invalidateHttpSession(true) // 세션 무효화
                                         .deleteCookies("JSESSIONID") // 쿠키 삭제
                                         .clearAuthentication(true) // 인증 정보 클리어
                 );
 
         //세션 설정 항상 stateless하게 관리
-/*
         http
                 .sessionManagement((session) -> session
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
-*/
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         //필터체인 연결
 /*        http
@@ -99,6 +110,10 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
+    }
+
+    private OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+        return new CustomOAuth2UserService();
     }
 }
 
