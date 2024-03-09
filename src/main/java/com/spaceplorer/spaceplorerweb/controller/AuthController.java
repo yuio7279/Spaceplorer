058211aad89b86d384ccd24d066e0447e5b17d76
yuio7279 <yuio7279@gmail.com>
2024-03-09 16:48:19 +0900

    Feat : 로그 기록 추가
    -
    
    -

--- src/main/java/com/spaceplorer/spaceplorerweb/controller/AuthController.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/controller/AuthController.java
@@ -1,36 +1,44 @@
 package com.spaceplorer.spaceplorerweb.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/auth")
-@RequiredArgsConstructor
+@Slf4j
 public class AuthController {
-    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService;
-
     @GetMapping("/login")
     public String login(){
 
+        log.debug("move to login page ");
         return "login";
     }
     @ResponseBody
     @GetMapping("/login/success")
-    public String loginSuccess(@AuthenticationPrincipal UserDetails userDetails){
-        return "success";
+    public String loginSuccess(){
+
+        log.debug("Login success ");
+        return "Login success";
     }
     @ResponseBody
     @GetMapping("/login/fail")
     public String loginFail(){
+        log.debug("login Fail");
+        return "login Fail";
+    }
+    @ResponseBody
+    @GetMapping("/logout/success")
+    public String logoutSuccess(){
 
-        return "fail";
+        log.debug("Logout success ");
+        return "Logout success";
+    }
+    @ResponseBody
+    @GetMapping("/logout/fail")
+    public String logoutFail(){
+
+        log.debug("Logout fail ");
+        return "Logout fail";
     }
 }
