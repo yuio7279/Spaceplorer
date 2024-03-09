c80d765427d440ea1fb1f13dbec021bf7b08fc20
yuio7279 <yuio7279@gmail.com>
2024-03-09 20:22:37 +0900

    Refactor : 불필요한 코드 삭제
    - 응답객체로 데이터를 넣기로 변경했기때문에 정보DTO에 불필요한 필드 및 생성자 주입을 삭제했다.
    
    -

--- src/main/java/com/spaceplorer/spaceplorerweb/dto/response/UserResponseDto.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/dto/response/UserResponseDto.java
@@ -1,6 +1,5 @@
 package com.spaceplorer.spaceplorerweb.dto.response;
 
-import com.spaceplorer.spaceplorerweb.common.ApiResponseDto;
 import com.spaceplorer.spaceplorerweb.domain.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -16,20 +15,14 @@ public class UserResponseDto {
     private final String email;
     private final String profileImageUrl;
 
-    //ApiResponseDto객체로 주입받아 저장.
-    private final String message;
-    private final Integer statusCode;
 
     //유저 조회
-    public UserResponseDto(User user, ApiResponseDto apiResponseDto) {
+    public UserResponseDto(User user) {
         this.id = user.getId();
         this.userId = user.getUserId();
         this.socialId = user.getSocialId();
         this.socialProvider = user.getSocialProvider();
         this.email = user.getEmail();
         this.profileImageUrl = user.getProfileImageUrl();
-
-        this.message = apiResponseDto.getMessage();
-        this.statusCode = apiResponseDto.getStatusCode();
     }
 }
