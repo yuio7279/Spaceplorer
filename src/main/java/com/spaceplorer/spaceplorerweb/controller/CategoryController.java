c8755856ea6a5ec0423e2f92eabcbfd01d6da2ec
yuio7279 <yuio7279@gmail.com>
2024-03-13 02:06:31 +0900

    Refactor : 카테고리 엔드포인트 간결하게 수정
    -
    
    -

--- src/main/java/com/spaceplorer/spaceplorerweb/controller/CategoryController.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/controller/CategoryController.java
@@ -17,14 +17,14 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     //카테고리 생성
-    @PostMapping("/category/{category_name}")
+    @PostMapping("/{category_name}")
     public ResponseEntity<ApiResponseDto<CategoryResponseDto>> createCategory(@PathVariable("category_name") String categoryName){
 
 
         return categoryService.createCategory(categoryName);
     }
     //카테고리 제거
-    @DeleteMapping("/category/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponseDto<CategoryResponseDto>> deleteCategory(@PathVariable("id") Long id){
         return categoryService.deleteCategory(id);
     }
