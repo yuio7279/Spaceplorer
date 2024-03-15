17bcb0ede9a4640efba2787723ccb860de89f762
yuio7279 <yuio7279@gmail.com>
2024-03-15 20:07:51 +0900

    Feat : 영수증 id로 영수증 조회기능 개발
    -
    
    -

--- src/main/java/com/spaceplorer/spaceplorerweb/controller/ReceiptController.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/controller/ReceiptController.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 /**
  *  옵션은 쿼리파라미터방식을 사용
@@ -22,15 +24,23 @@ import org.springframework.web.bind.annotation.*;
 public class ReceiptController {
     private final ReceiptService receiptService;
 
+    //영수증 생성
     @PostMapping()
     public ResponseEntity<ApiResponseDto<ReceiptResponseDto>> createReceipt(@RequestBody ReceiptRequestDto requestDto){
         return receiptService.createReceipt(requestDto);
     }
-    @GetMapping("/api/receipts/{receipt_id}")
+    //영수증 조회
+    @GetMapping("/{receipt_id}")
     public ResponseEntity<ApiResponseDto<ReceiptResponseDto>> getReceiptById(@PathVariable("receipt_id") Long id){
-        //receiptService.getReceiptById(id);
-        log.info("[getReceiptById:]");
 
-        return null;
+        log.info("[getReceiptById:]");
+        return receiptService.getReceiptById(id);
+    }
+    //회원 별 영수증 조회
+    @GetMapping()
+    public ResponseEntity<ApiResponseDto<List<ReceiptResponseDto>>> getReceiptListByUser(){
+
+        log.info("[getReceiptById:]");
+        return receiptService.getReceiptListByUser();
     }
 }
