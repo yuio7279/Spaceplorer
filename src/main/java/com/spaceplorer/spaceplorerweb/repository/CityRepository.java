e08709cbbabd4bc280ea86762759f013fb0f00e6
yuio7279 <yuio7279@gmail.com>
2024-03-16 23:37:50 +0900

    Feat: 게시글 작성 시, 태그관련 메서드
    - 카테고리이름과 도시명으로 조회

--- src/main/java/com/spaceplorer/spaceplorerweb/repository/CityRepository.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/repository/CityRepository.java
@@ -11,4 +11,7 @@ public interface CityRepository extends JpaRepository<City, Long> {
     @Query("SELECT c FROM City c WHERE c.planet.id = :planetId AND c.id = :cityId")
     Optional<City> findByPlanetIdAndCityId(@Param("planetId") Long pId, @Param("cityId") Long cId);
 
+
+
+    Optional<City> findByPlanetAliasAndCityName(String planetName, String cityName);
 }
