7994e03b9b1f0ea590462ce61bdde33045438db3
yuio7279 <yuio7279@gmail.com>
2024-03-20 05:36:57 +0900

    Feat: 레디스 사용 설정
    - setKeySerializer : 키의 직렬화 방식을 선택한다.
    - setValueSerializer : 값의 직렬화 방식을 선택한다.
    - 두가지 모두 문자열 처리용을 선택했다.

--- src/main/java/com/spaceplorer/spaceplorerweb/config/BeanConfig.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/config/BeanConfig.java
@@ -3,6 +3,9 @@ package com.spaceplorer.spaceplorerweb.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
@@ -14,6 +17,15 @@ public class BeanConfig {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
     }
 
 }
