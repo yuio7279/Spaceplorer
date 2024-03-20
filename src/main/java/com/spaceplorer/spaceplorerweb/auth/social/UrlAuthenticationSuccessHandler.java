dc8c180c78836510ce0782c5d171a2cc27ba60d3
yuio7279 <yuio7279@gmail.com>
2024-03-20 19:04:54 +0900

    Refactor: 메서드들을 묶는 메서드패턴 사용
    - 로그인 성공 시, jti를 제거하는 메서드 또한 추가하였다.

--- src/main/java/com/spaceplorer/spaceplorerweb/auth/social/UrlAuthenticationSuccessHandler.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/auth/social/UrlAuthenticationSuccessHandler.java
@@ -5,6 +5,7 @@ import com.spaceplorer.spaceplorerweb.auth.jwt.TokenProvider;
 import com.spaceplorer.spaceplorerweb.domain.Role;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -27,19 +28,31 @@ public class UrlAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucc
     private final RefreshTokenProvider refreshTokenProvider;
 
     @Override
+    @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        String userName = authentication.getName();
-        Role role = getRole(authentication);
+        try{
+            String userName = authentication.getName();
+            Role role = getRole(authentication);
 
-        String createdToken = tokenProvider.createToken(userName, role);
-        String refreshToken = refreshTokenProvider.createRefreshToken(userName);
-        refreshTokenProvider.saveRefreshToken(userName, refreshToken);
-
-        tokenProvider.addJwtToCookie(createdToken,response);
-        log.info("[Complete to login:{}, {}]",userName, role);
-        response.sendRedirect("/login/success");
+            issueTokenProcess(response, userName, role);
+            log.info("[Complete to login:{}, {}]",userName, role);
+            response.sendRedirect("/login/success");
+        } catch (Exception e){
+            log.error("[Exception occurred during issuance:{}]",e.getMessage());
+        }
     }
 
+    //토큰 발급을 하나로 묶었다.
+    private void issueTokenProcess(HttpServletResponse response, String userName, Role role) throws Exception {
+            String createdToken = tokenProvider.createToken(userName, role);
+            String refreshToken = refreshTokenProvider.createRefreshToken(userName);
+            refreshTokenProvider.saveRefreshToken(userName, refreshToken);
+            tokenProvider.addJwtToCookie(createdToken, response);
+
+            //로그인 시 jti를 제거해 리프레시 토큰에 요청을 받을 수 있게 한다.
+            refreshTokenProvider.removeRefreshTokenJti(userName);
+            log.info("[Login success:{}]", userName);
+    }
     private Role getRole(Authentication authentication) {
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         String role = authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
