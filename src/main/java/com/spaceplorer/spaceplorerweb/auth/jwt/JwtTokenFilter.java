0bf6e88ce7538dc5f27b5afe093d55377e02be4f
yuio7279 <yuio7279@gmail.com>
2024-03-20 18:57:39 +0900

    Feat: refreshToken 으로 accessToken 재발급 기능 개발
    1. 토큰이 만료된 경우, 리프레시 토큰이 만료되었는지 판별
    2. 리포레시 토큰은 보안을 위해 1회만 재발급을 해준다.
    3. 이를 위해 식별자 jti를 검증한다. Redis에 이미 존재할 경우, 중복 발급요청이기 때문에 거절한다.
    4. 동시에 Redis에 있던 리프레시토큰과 jti 를 제거한다.
    5. 재발급 요청이 1회만 온 경우, 엑세스토큰을 재발급해준다. 이때 jti 를 Redis에 저장한다.

--- src/main/java/com/spaceplorer/spaceplorerweb/auth/jwt/JwtTokenFilter.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/auth/jwt/JwtTokenFilter.java
@@ -1,7 +1,9 @@
 package com.spaceplorer.spaceplorerweb.auth.jwt;
 
 
+import com.spaceplorer.spaceplorerweb.domain.Role;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +20,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.spaceplorer.spaceplorerweb.common.Messages.EXPIRED_TOKEN;
+
 //토큰을 검증하는 필터
 @RequiredArgsConstructor
 @Component
@@ -26,8 +30,9 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
 
     private final TokenProvider tokenProvider;
+    private final RefreshTokenProvider refreshTokenProvider;
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, ExpiredJwtException {
         try {
 
             String token_ = tokenProvider.getTokenFromRequest(request);
@@ -54,11 +59,64 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                     filterChain.doFilter(request, response);
                 }
             }
-        }catch (Exception e){
-            log.error("[error :{}]",e.getMessage());
+            //Jwt 토큰이 만료된 경우,
+        }catch (ExpiredJwtException e){
+                String userName = e.getClaims().getSubject();
+                log.debug(EXPIRED_TOKEN+" {}...", userName );
+                log.debug("[Looking for refresh token..]");
+
+                Role role = Role.valueOf((String)e.getClaims().get("auth"));
+
+                //리프레시 토큰 만료 체크 & 토큰이 존재하는지도 확인
+                boolean notExpired = refreshTokenProvider.validateRedisRefreshToken(userName);
+
+                //만료됨
+                if(!notExpired){
+                    log.debug("[Must be re-login]");
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+
+                //리프레시 토큰에 중복 요청 된건지 확인, jti가 존재하면 이는 중복 요청이다. 거부한다.
+                boolean isJtiPresent = refreshTokenProvider.isPresentJtiAtRedis(userName);
+                if(isJtiPresent){
+                    log.debug("[Already issued access token key:{}]",userName);
+                    log.info("[Can not issue access token key:{}]",userName);
+
+                    refreshTokenProvider.removeRefreshTokenJti(userName);
+                    refreshTokenProvider.removeRefreshToken(userName);
+
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+
+
+                try {
+                    log.debug("[Re issue in progress...]");
+                    this.reIssueTokenProcess(response, userName, role);
+
+                    filterChain.doFilter(request, response);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+
+
+
         }
     }
 
+    private void reIssueTokenProcess(HttpServletResponse response, String userName, Role role) throws Exception {
+        String createdToken = tokenProvider.createToken(userName, role);
+
+        //리프레시 토큰또한 재발급 한다.
+        String refreshToken = refreshTokenProvider.createRefreshToken(userName);
+        refreshTokenProvider.saveRefreshToken(userName, refreshToken);
+
+        //refresh token이 중복 사용됬는지 확인용 jti 저장
+        refreshTokenProvider.saveRefreshTokenJti(userName, refreshToken);
+        log.info("[Complete to issue new access token]");
+        tokenProvider.addJwtToCookie(createdToken, response);
+    }
     private static List<SimpleGrantedAuthority> getRoleList(String role) {
         List<SimpleGrantedAuthority> grantedAuthorityList = new ArrayList<>();
         grantedAuthorityList.add(new SimpleGrantedAuthority(role));
