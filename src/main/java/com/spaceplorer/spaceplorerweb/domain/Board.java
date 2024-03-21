d409fbf635b8e5e323d864981c6010557a72babc
yuio7279 <yuio7279@gmail.com>
2024-03-21 16:05:50 +0900

    Feat: Board 엔티티 연관관계 및 필드 추가
    - 추천수, 카테고리, 이미지리스트에 해당하는 생성자또한 수정하였다.
    
    - @EntityListeners(AuditingEntityListener.class)를 추가
    
     생성시간(createdTime), 수정시간(modifiedTme)을 자동으로 기록하게끔 했다.

--- src/main/java/com/spaceplorer/spaceplorerweb/domain/Board.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/domain/Board.java
@@ -3,10 +3,18 @@ package com.spaceplorer.spaceplorerweb.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "board")
 public class Board {
 
@@ -22,25 +30,61 @@ public class Board {
     private String content;
 
     @Column(nullable = false)
-    private Long views = 0L;
+    private Long views;
 
-/*    //작성자
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;*/
+    private Integer likeCount;
 
-    @ManyToOne
+    //상세 게시글 조회 시에만 필요하다.
+    //imageUrl
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> imageList = new ArrayList<>();
+
+    //userName, thumbnail
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "city_id")
     private City city;
 
+    //게시글 리스트 조회 시에만 필요하다.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 
-    public Board(String title, String content,/*User user*/ City city) {
+    //생성 시간
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdTime;
+
+    //수정 시간
+    @LastModifiedDate
+    private LocalDateTime modifiedTime;
+
+    public Board(String title, String content, City city, User user, Category category) {
         this.title = title;
         this.content = content;
-        //this.user = user;
+        this.user = user;
         this.city = city;
+        this.category = category;
+        this.likeCount = 0;
+        this.views = 0L;
     }
 
+    //변경감지 더리체킹
     public void incrementViews(){
         views += 1;
+    }
+
+    public void addImage(Image image){
+        this.imageList.add(image);
+        image.setBoard(this);
+    }
+
+    //변경감지 더리체킹
+    public void setLikeCount(Integer likeCount){
+        this.likeCount = likeCount;
     }
 }
