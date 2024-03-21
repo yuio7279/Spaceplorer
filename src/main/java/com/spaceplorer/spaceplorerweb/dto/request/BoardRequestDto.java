3f695641f16c3162c571e8112f050f05a52b032a
yuio7279 <yuio7279@gmail.com>
2024-03-21 16:34:54 +0900

    Refactor: BoardRequestDto 접근자 private으로 수정

--- src/main/java/com/spaceplorer/spaceplorerweb/dto/request/BoardRequestDto.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/dto/request/BoardRequestDto.java
@@ -4,17 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class BoardRequestDto {
 
-    public String title;
-    public String content;
+    private String title;
+    private String content;
 
     //카테고리 이름으로 게시판 속성을 확인
-    public String categoryName;
+    private String categoryName;
 
-    public String cityName;
+    private String cityName;
+
+    private List<String> imageUrlList;
 
 }
