274e618afd04e8ec16e6ce3041bd3ee95722dc6a
yuio7279 <yuio7279@gmail.com>
2024-03-21 16:27:06 +0900

    Feat: BoardController 카테고리별 게시글들조회[페이지네이션], 게시글등록, 게시글 삭제, 추천, 추천취소
    - getBoardListByCategoryName()
    카테고리별 게시글을 조회한다. 카테고리명, 파라미터로 페이지, 출력할개수를 받는다.
    
    - 게시글 등록
    @AuthenticationPrincipal을 이용해 유저정보를 가져와 게시글을 등록한다.
    https://dao-blog.tistory.com/97
    
    - likeBoard, likeCancelBoard
    @AuthenticationPrincipal을 사용해 유저정보를 가져오고 이를 토대로 추천, 추천취소를 할 수 있다.

--- src/main/java/com/spaceplorer/spaceplorerweb/controller/BoardController.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/controller/BoardController.java
@@ -1,12 +1,17 @@
 package com.spaceplorer.spaceplorerweb.controller;
 
 import com.spaceplorer.spaceplorerweb.dto.request.BoardRequestDto;
-import com.spaceplorer.spaceplorerweb.dto.response.BoardResponseDto;
 import com.spaceplorer.spaceplorerweb.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 게시판은 인터렉티브할 필요가 없기때문에, ThymeLeaf를 사용하여 SSR로 개발해보기
@@ -17,14 +22,19 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/boards")
+@Slf4j
 public class BoardController {
 
     private final BoardService boardService;
 
-    //전체 게시판 리스트를 출력
+    //카테고리별 게시판 리스트를 출력
     @GetMapping()
-    public String getBoardList(Model model){
-        model.addAttribute("boardList",boardService.getBoardList());
+    public String getBoardListByCategoryName(
+            @RequestParam("category_name") String categoryName,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value= "pageSize", defaultValue = "20") int pageSize,
+                               Model model){
+        model.addAttribute("boardPage", boardService.getBoardListByCategoryName(categoryName, page, pageSize));
         return "board_list";
     }
 
@@ -37,21 +47,44 @@ public class BoardController {
         model.addAttribute("board",boardService.getBoardById(id));
         return "board_detail";
     }
+
     //게시글 등록
     @PostMapping
-    public String createBoard(@RequestBody BoardRequestDto boardRequestDto){
+    public String createBoard(@RequestBody BoardRequestDto boardRequestDto,
+                              @AuthenticationPrincipal String username,
+                              RedirectAttributes redirectAttributes){
+        log.info("[username:{}]",username);
+        Long boardId= boardService.createBoard(boardRequestDto, Long.valueOf(username));
 
-        BoardResponseDto boardResponseDto = boardService.createBoard(boardRequestDto);
-        return "redirect:/boards/"+boardResponseDto.getId();
+
+        return "redirect:/boards/"+boardId;
     }
 
     //게시글 삭제
     @DeleteMapping("/{board_id}")
-    public String deleteBoardById(@PathVariable("board_id") Long id){
-        boardService.deleteBoardById(id);
-        return "redirect:/boards";
+    public String deleteBoardById(
+            @PathVariable("board_id") Long id,
+            @AuthenticationPrincipal String username){
+        String categoryName = boardService.deleteBoardById(id, Long.valueOf(username));
+        String encode = URLEncoder.encode(categoryName, StandardCharsets.UTF_8);
+        return "redirect:/boards?category_name="+encode;
     }
 
-
+    //추천하기
+    @PostMapping("/{board_id}/like")
+    public String likeBoard(
+            @PathVariable("board_id") Long id,
+            @AuthenticationPrincipal String username){
+        boardService.likeBoard(id, Long.valueOf(username));
+        return "redirect:/boards/"+id;
+    }
+    //추천취소
+    @DeleteMapping("/{board_id}/like")
+    public String likeCancelBoard(
+            @PathVariable("board_id") Long id,
+            @AuthenticationPrincipal String username){
+        boardService.likeCancelBoard(id, Long.valueOf(username));
+        return "redirect:/boards/"+id;
+    }
 
 }
