c472678159ce2b6ac00b95df3c5e2489984b5421
yuio7279 <yuio7279@gmail.com>
2024-03-21 16:33:45 +0900

    Feat: BoardRepository 메서드 추가
    - findAllByCategoryName
    카테고리별로 게시글들을 가져온다. Page객체를 리턴한다.
    Pageable과 FETCH JOIN을 동시에 사용할 경우, hibernate에서 제한을 걸어놓는다. 이 경우 countQuery 옵션을 통해 따로 개수를 세주는 쿼리를 추가해주어야 한다.
    
    게시글 리스트를 조회하기 때문에 N+1 문제가 발생하기 때문에, 즉시 로딩을 위한 FETCH JOIN을 사용했다.
    
    - findByIdAndSocialId
    socialId(식별자)와 게시글 id로 게시글을 가져온다. 글을 삭제하거나 수정할 때에 사용한다.

--- src/main/java/com/spaceplorer/spaceplorerweb/repository/BoardRepository.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/repository/BoardRepository.java
@@ -1,7 +1,29 @@
 package com.spaceplorer.spaceplorerweb.repository;
 
 import com.spaceplorer.spaceplorerweb.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
+
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    //카테고리가 LAZY LOADING 이므로 N+1 문제를 막기 위해 즉시 로딩한다.
+    //이를 위한 FETCH JOIN을 사용했다.
+
+    //Pageable과 FETCH JOIN을 동시에 사용할 경우, hibernate에서 제한때문에 사용을 막는다. 이런 경우, countQuery 옵션을 통해 따로 생성해주어야 한다.
+    @Query(value = "SELECT b FROM Board b " +
+            "LEFT JOIN FETCH b.category c " +
+            "WHERE c.categoryName = :categoryName",
+            countQuery = "SELECT count(b) FROM Board b LEFT JOIN b.category c WHERE c.categoryName = :categoryName")
+    Page<Board> findAllByCategoryName(@Param("categoryName") String categoryName, Pageable pageable);
+
+
+    @Query(value = "SELECT b FROM Board b WHERE b.id = :id AND b.user.socialId = :socialId")
+    Optional<Board> findByIdAndSocialId(@Param("id") Long id, @Param("socialId") Long socialId);
 }
