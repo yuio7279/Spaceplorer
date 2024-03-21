63f3dfff760f2ba55581b4b335f87ac7c282b9fa
yuio7279 <yuio7279@gmail.com>
2024-03-21 16:51:46 +0900

    Refactor: SocialUserDetails getPhone() 제거
    - 필요하지 않을 것 같아 제거했다.

--- src/main/java/com/spaceplorer/spaceplorerweb/auth/social/userdetails/SocialUserDetails.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/auth/social/userdetails/SocialUserDetails.java
@@ -1,13 +1,11 @@
 package com.spaceplorer.spaceplorerweb.auth.social.userdetails;
 
-public interface SocialUserDetails{
+public interface SocialUserDetails {
     public Long getSocialId();
 
     public String getUserName();
 
     public String getEmail();
-
-    public String getPhone();
 
     public String getThumbnail();
 
