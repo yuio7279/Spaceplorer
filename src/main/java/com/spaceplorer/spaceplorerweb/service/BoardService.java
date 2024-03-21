07b14d2712d9cb1c83b8d45b1bf751cb5efa7f81
yuio7279 <yuio7279@gmail.com>
2024-03-21 16:44:24 +0900

    Feat: BoardService 기능 개발
    - getBoardListByCategoryName()
    카테고리별 게시글을 가져온다. Page객체로 리턴하고 파라미터로 현재페이지, 보여줄 개수를 받는다.
    
    - getBoardById()
    게시글 1개 조회이긴 하지만, LAZY LOADING이기 때문에 Transactional(readOnly = true) 어노테이션을 달아 성능 최적화와 데이터 일관성을 보장할 수 있게 되었다.
    
    - createBoard
    게시글을 생성하는 작업과, 영속성전파로 이미지엔티티를 만드는 작업을 같이 병행하기에 @Transactional을 사용했다.
    
    - likeBoard, likeCancelBoard
    추천, 추천취소이다. 따로 likes 테이블을 만들어 어떤 유저가 어떤 게시글을 추천했는지 확인할 수 있다. 여러 작업이 병행하므로 @Transacetional을 넣어주었다.

--- src/main/java/com/spaceplorer/spaceplorerweb/service/BoardService.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/service/BoardService.java
@@ -1,19 +1,22 @@
 package com.spaceplorer.spaceplorerweb.service;
 
-import com.spaceplorer.spaceplorerweb.domain.Board;
-import com.spaceplorer.spaceplorerweb.domain.City;
+import com.spaceplorer.spaceplorerweb.domain.*;
 import com.spaceplorer.spaceplorerweb.dto.request.BoardRequestDto;
+import com.spaceplorer.spaceplorerweb.dto.response.BoardDetailResponseDto;
 import com.spaceplorer.spaceplorerweb.dto.response.BoardResponseDto;
-import com.spaceplorer.spaceplorerweb.repository.BoardRepository;
-import com.spaceplorer.spaceplorerweb.repository.CityRepository;
+import com.spaceplorer.spaceplorerweb.repository.*;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
 import java.util.Optional;
 
 import static com.spaceplorer.spaceplorerweb.common.Messages.FAIL_TO_CREATE_BOARD;
@@ -28,22 +31,27 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
     private final CityRepository cityRepository;
+    private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
+    private final LikeRepository likeRepository;
 
+    //게시글 리스트 조회 20개씩
+    public Page<BoardResponseDto> getBoardListByCategoryName(String categoryName, int page, int pageSize) {
+        Pageable pageable = PageRequest.of(
+                page, pageSize, Sort.by(Sort.Direction.DESC, "id")); // 페이지 번호, 사이즈, 정렬 기준
 
-    //게시글 리스트 조회
-    public List<BoardResponseDto> getBoardList() {
-        List<Board> boardEntityList = boardRepository.findAll();
-        if(boardEntityList.isEmpty()){
+        Page<Board> boardPage = boardRepository.findAllByCategoryName(categoryName, pageable);
+        if(boardPage.isEmpty()){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, NOT_FOUND_BOARD);
         }
-        List<BoardResponseDto> responseDto = boardEntityList.stream().map(BoardResponseDto::new).toList();
-        log.debug("[Found BoardResponseDto:{}]",responseDto);
 
-        return responseDto;
+        return boardPage.map(BoardResponseDto::new);
     }
 
     //게시글 1개 조회, 게시글 클릭 시
-    public BoardResponseDto getBoardById(Long id) {
+    //LAZY LOADING시, Transactional로 처리를 해야 안전하게 로딩한다. + incrementViewsAsync
+    @Transactional(readOnly = true)
+    public BoardDetailResponseDto getBoardById(Long id) {
         Optional<Board> entity_ = boardRepository.findById(id);
         if(entity_.isEmpty()){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, NOT_FOUND_BOARD);
@@ -51,52 +59,109 @@ public class BoardService {
         Board entity = entity_.get();
         //조회 수 증가
         incrementViewsAsync(entity);
-        BoardResponseDto boardResponseDto = new BoardResponseDto(entity);
-        log.debug("[Found BoardResponseDto:{}]",boardResponseDto);
+        BoardDetailResponseDto boardResponseDto = new BoardDetailResponseDto(entity);
+        log.debug("[Created BoardDetailResponseDto:{}]",boardResponseDto);
 
         return boardResponseDto;
     }
 
     //게시글 생성
-    public BoardResponseDto createBoard(BoardRequestDto boardRequestDto) {
-        log.debug("[Board Request title:{} category:{} city:{}]"
-                ,boardRequestDto.getTitle(),
+    @Transactional
+    public Long createBoard(BoardRequestDto boardRequestDto, Long socialId) {
+        log.debug("[Creating board... Request title:{} category:{} city:{}]"
+                , boardRequestDto.getTitle(),
                 boardRequestDto.getCategoryName(),
                 boardRequestDto.getCityName());
 
+        City city = cityRepository.findByPlanetAliasAndCityName(boardRequestDto.getCategoryName(), boardRequestDto.getCityName())
+                .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST, FAIL_TO_CREATE_BOARD + " - city not found"));
 
-        Optional<City> city = cityRepository.findByPlanetAliasAndCityName(boardRequestDto.getCategoryName(), boardRequestDto.getCityName());
-        if(city.isEmpty()){
-                throw new ResponseStatusException(BAD_REQUEST, FAIL_TO_CREATE_BOARD);
+        User user = userRepository.findBySocialId(socialId)
+                .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST, FAIL_TO_CREATE_BOARD + " - user not found"));
+
+        Category category = categoryRepository.findByCategoryName(boardRequestDto.getCategoryName())
+                .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST, FAIL_TO_CREATE_BOARD + " - Category not found"));
+
+        Board board = new Board(boardRequestDto.getTitle(), boardRequestDto.getContent(), city, user, category);
+        Board savedBoard = boardRepository.save(board);
+
+
+        if (boardRequestDto.getImageUrlList() == null || boardRequestDto.getImageUrlList().isEmpty()) {
+            log.debug("[Created board id:{}]",savedBoard.getId());
+            return savedBoard.getId();
+        }else{
+            //이미지가 있을 경우
+            for (String imageUrl : boardRequestDto.getImageUrlList()) {
+                if (imageUrl != null && !imageUrl.trim().isEmpty()) {
+                    Image image = new Image(imageUrl, savedBoard);
+                    savedBoard.addImage(image);
+                }
+            }
+
         }
-
-        //게시글 생성
-        Board board = new Board(boardRequestDto.getTitle(), boardRequestDto.getContent(), city.get());
-        Board entity = boardRepository.save(board);
-
-        //dto
-        return new BoardResponseDto(entity);
-
+        log.debug("[Created board id:{}]",savedBoard.getId());
+        return savedBoard.getId();
     }
+
 
     //게시글 삭제
-    public void deleteBoardById(Long id) {
-        Optional<Board> entity_ = boardRepository.findById(id);
-        if(entity_.isEmpty()){
+    public String deleteBoardById(Long id, Long socialId) {
+        Optional<Board> entity = boardRepository.findByIdAndSocialId(id, socialId);
+        if(entity.isEmpty()){
             throw new ResponseStatusException(NOT_FOUND, NOT_FOUND_BOARD);
         }
-        boardRepository.delete(entity_.get());
+        boardRepository.delete(entity.get());
         log.info("[Delete board complete:{}]",id);
+        return entity.get().getCategory().getCategoryName();
     }
 
 
-    //조회수 증가
+    //조회수 증가, 더티체킹
     @Async
-    //동시성 제어 기법을 공부하기
     public void incrementViewsAsync(Board board) {
         board.incrementViews();
         boardRepository.save(board);
     }
 
+    //게시글 추천
+    @Transactional
+    public void likeBoard(Long boardId, Long socialId) {
+        Board boardEntity = boardRepository.findById(boardId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, NOT_FOUND_BOARD));
+        User userEntity = userRepository.findBySocialId(socialId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "User not found"));
 
+        //중복 추천 방지
+        Optional<Like> likeEntity = likeRepository.findByBoardIdAndSocialId(boardId, socialId);
+        if(likeEntity.isPresent()){
+            return;
+        }
+        likeRepository.save(new Like(userEntity, boardEntity));
+
+        // like 수를 계산
+        int likeCount = likeRepository.countByBoardId(boardId);
+        boardEntity.setLikeCount(likeCount);
+        boardRepository.save(boardEntity);
+    }
+
+
+    //게시글 추천취소
+    @Transactional
+    public void likeCancelBoard(Long boardId, Long socialId) {
+        Optional<Like> like = likeRepository.findByBoardIdAndSocialId(boardId, socialId);
+        if(like.isEmpty()){
+            return;
+        }
+
+        Board boardEntity = boardRepository.findById(boardId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, NOT_FOUND_BOARD));
+
+        likeRepository.delete(like.get());
+
+        // Like 삭제 후 변경된 like 수를 업데이트
+        int likeCount = likeRepository.countByBoardId(boardId);
+        boardEntity.setLikeCount(likeCount);
+
+        boardRepository.save(boardEntity);
+    }
 }
