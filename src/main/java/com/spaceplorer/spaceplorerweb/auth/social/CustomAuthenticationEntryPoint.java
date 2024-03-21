28d1e680b2a8ed857e1dd00a147ec86dfefd0cf3
yuio7279 <yuio7279@gmail.com>
2024-03-21 16:01:48 +0900

    Refactor: 인증 실패 시, 호출되는 EntryPoint 에러핸들링 리다렉트 uri 수정

--- src/main/java/com/spaceplorer/spaceplorerweb/auth/social/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/spaceplorer/spaceplorerweb/auth/social/CustomAuthenticationEntryPoint.java
@@ -11,18 +11,16 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-//인증 실패 등을 처리하기 위함
+//인증 실패를 처리하기 위한 EntryPoint
 @Component
 @Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        // 에러 메시지를 쿼리 파라미터나 세션으로 전달
-        // 여기서는 쿼리 파라미터를 사용하는 예시입니다.
-        String errorMessage = "로그인 후 이용해주세요.";
-        log.info("[:{}]",authException.toString());
+
+        String errorMessage = "로그인이 필요합니다.\n\n로그인 후 이용해주세요.";
         String encodedMessage = URLEncoder.encode(errorMessage, StandardCharsets.UTF_8);
-        response.sendRedirect("/errors?message=" + encodedMessage);
+        response.sendRedirect("/errors/401/error?=" + encodedMessage);
     }
 }
